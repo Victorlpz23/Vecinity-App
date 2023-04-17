a132ba813e5e701ff485d97bd21ad233f2ac3b1a
Victor Lopez <victorlpz2301@gmail.com>
2023-04-17 14:25:23 +0200

    feat: configure db connection

---
+++ api/config/db.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI 
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.info(`Succesfully connect to the database`))
+  .catch((error) => console.error('An error occurred trying to connect to the database', error))
+
