c2f94925507609e25a10b7ed214c02eab1005e94
Victor Lopez <victorlpz2301@gmail.com>
2023-04-17 13:07:34 +0200

    Starting Api

---
+++ api/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Application is running at port ${port}`))
