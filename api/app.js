d379c839a526c0f5b780c13b19528a25f5ab45fa
Victor Lopez <victorlpz2301@gmail.com>
2023-04-17 13:51:00 +0200

    feat: api error handling

--- api/app.js
+++ api/app.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
 
 const express = require('express');
+const logger = require('morgan');
+const createError = require('http-errors');
 
 const app = express();
 
+app.use(logger('dev'));
 
+// Error Handling
+app.use((req, res, next) => next(createError(404, 'Route not found')));
 
+app.use((error, req, res, next) => {
+  if (!error.status) {
+    createError(500, error)
+  }
+
+  const data = {
+    message: error.message
+  }
+  console.error(error)
+
+  res.status(error.status)
+    .json(data)
+});
 
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Application is running at port ${port}`))
+app.listen(port, () => console.info(`Application is running at port ${port}`))
