76211fed7725fc19688227ac17198b3c74f65e7b
Victor Lopez <victorlpz2301@gmail.com>
2023-04-19 10:46:41 +0200

    feat: Community CRUD Done

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const communitiesControllers = require('../controllers/communities.controllers');
+const communitiesMid = require('../middlewares/communities.mid');
 
 
-router.get('/communities', communitiesControllers.list),
-router.post('/communities', communitiesControllers.create),
-router.get('/communities/:id', communitiesControllers.detail),
-router.patch('/communities/:id', communitiesControllers.update),
-router.delete('/communities/:id', communitiesControllers.delete),
+router.get('/communities', communitiesControllers.list);
+router.post('/communities', communitiesControllers.create);
+// router.post('/communities/:id/join', communitiesControllers.join)
+router.get('/communities/:id', communitiesMid.exists, communitiesControllers.detail);
+router.patch('/communities/:id', communitiesMid.exists, communitiesControllers.update);
+router.delete('/communities/:id', communitiesMid.exists, communitiesControllers.delete);
 
-module.exports = router
+module.exports = router;
 
 
---
+++ api/middlewares/communities.mid.js
@@ -0,0 +1,18 @@
+const Community = require('../models/community.model');
+const createError = require('http-errors');
+
+
+
+module.exports.exists = (req, res, next) => {
+  // const communityId = req.params.communityId || req.params.id 
+  Community.findById(req.params.id)
+    .then((community) => {
+      if (community) {
+        req.community = community;
+        next();
+      } else {
+        next(createError(404, 'Community not found'));
+      }
+    })
+    .catch(next);
+};
