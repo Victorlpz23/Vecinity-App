48cb321fb057bc891f40c7aa5cc3329a849bac5c
Victor Lopez <victorlpz2301@gmail.com>
2023-04-24 19:54:52 +0200

    Navbar Component

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import CommunitiesList from "./components/communities/communities-list/CommunitiesList";
+import Home from "./pages/Home";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
   return (
     <>
-    <CommunitiesList />
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/communities" element={<CommunitiesList />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
     </>
   );
 }
