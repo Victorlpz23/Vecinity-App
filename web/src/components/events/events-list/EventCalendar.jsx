f127a01c2e7ab825e3f03ccdf16e7b6cc39a2290
Victor Lopez <victorlpz2301@gmail.com>
2023-05-01 11:40:08 +0200

    feat: Bottom Menu & Styles

--- web/src/components/events/events-list/EventCalendar.jsx
+++ web/src/components/events/events-list/EventCalendar.jsx
@@ -1,34 +1,63 @@
-import { useState, useEffect } from 'react';
-import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-import eventsService from '../../../services/events'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { useParams } from 'react-router-dom';
+import DateTimePicker from 'react-datetime-picker';
+
+
 
 const localizer = momentLocalizer(moment);
-2
-function EventCalendar() {
-  const { id } = useParams()
+
+const MyCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedEvent, setSelectedEvent] = useState(null);
   const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    const loadEvents = async () => {
-      const events = await eventsService.list(id);
-      setEvents(events);
+  const handleSelectSlot = (slotInfo) => {
+    const newEvent = {
+      start: slotInfo.start,
+      end: slotInfo.end,
+      title: 'Nuevo evento',
     };
-    loadEvents();
-  }, [id]);
+    setEvents([...events, newEvent]);
+  };
+
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
   return (
-    <>
-    <BigCalendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-    />
-    </>
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="w-full mb-4">
+        <DateTimePicker
+          value={selectedDate}
+          onChange={handleDateChange}
+          disableClock={true}
+          format="dd/MM/yyyy HH:mm"
+        />
+      </div>
+      <div className="w-full">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          onSelectEvent={handleSelectEvent}
+          selectable
+          onSelectSlot={handleSelectSlot}
+          defaultView="day"
+          views={['day', 'week', 'month']}
+          step={60}
+          timeslots={1}
+          min={new Date(2023, 0, 1, 8, 0)}
+          max={new Date(2023, 0, 1, 22, 0)}
+          scrollToTime={new Date(2023, 0, 1, 8, 0)}
+        />
+      </div>
+    </div>
   );
-}
+};
 
-export default EventCalendar;
+export default MyCalendar;
--- web/src/pages/CreateJoinPage.jsx
+++ web/src/pages/CreateJoinPage.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function CreateJoinPage() {
   return (
     <>
-      <Link to={'/join'} >
-      <h1>Join your community</h1>
-      </Link>
-
-      <Link to={'/communities/create'} >
-      <h1>Create your community</h1>
-      </Link>
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Welcome to Vecinity!
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Choose an option to get started
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <NavLink
+              to={'/join'}
+              className="flex items-center justify-center h-48 bg-white hover:bg-gray-100  text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow active:bg-orange"
+            >
+              <div>
+                <h3 className="text-xl">Join your Community</h3>
+                <p className="text-gray-500 mt-2 text-sm">
+                  Once you've joined your community you'll be able to submit claims, socialize with your neighbours and so much more!
+                </p>
+              </div>
+            </NavLink>
+            <NavLink
+              to={'/communities/create'}
+              className="flex items-center justify-center h-48 bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow active:bg-orange"
+            >
+              <div>
+                <h3 className="text-xl">Create a Community</h3>
+                <p className="text-gray-500 mt-2 text-sm">
+                  Are you a community manager? Start your community and invite the neighbours to join!
+                </p>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default CreateJoinPage
+export default CreateJoinPage;
